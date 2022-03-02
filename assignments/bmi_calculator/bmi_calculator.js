c23e2d151407da48e283b3fa723a2915493f1435
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-03-02 14:10:15 +0530

    BMICalculator submission

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,9 @@
 // This function should return the BMI for a person 
-function BMICalculator(mass, height) {
-    // Write your code here
+function BMICalculator(mass, height) //50 5 
+{
+    
 }
-
 module.exports = BMICalculator;
+
+
+// git checkout -b "BMICalculator"
