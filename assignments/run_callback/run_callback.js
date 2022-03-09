b71305670e2add54efaae23b02166f13764da9f1
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-03-09 11:09:45 +0530

    RunCallback submission

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,5 +1,13 @@
-function RunCallback(a, b, cb) {
-    // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
+function RunCallback(a, b, cb) { // a=10 , b=25 , cb=function(val){return val+100}
+    // 
+    
+    // Write you code here, you need to add a and b, 
+    // pass the result into callback function cb as argument return the result
+
+    let sum = a+b; // sum = 35
+    return cb(sum); // cb(35)=35+100=135
+
 }
+
 
 module.exports = RunCallback;
